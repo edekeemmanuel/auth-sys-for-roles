e2dd89b1b921d622043ef9b0a41fb07581f6e66e
Abiola Fasanya <hezekiahfasanya@gmail.com>
2021-11-18 00:14:29 +0100

    Update routes.js

--- modules/teacher/routes.js
+++ modules/teacher/routes.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 
 const teacher = require('./handler')
 
-router.get('/promote/:id', teacher.promote)
-router.get('/demote/:id', teacher.demote)
-router.post('/grade', teacher.grade)
-router.get('/students', teacher.students)
+router.get('/promote/:id', [auth, authTeacher], teacher.promote)
+router.get('/demote/:id', [auth, authTeacher], teacher.demote)
+router.post('/grade', [auth, authTeacher], teacher.grade)
+router.get('/students', [auth, authTeacher], teacher.students)
 
 module.exports = router
