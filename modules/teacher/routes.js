cb08d08678361df8312afdade72a5f667db32f25
Abiola Fasanya <hezekiahfasanya@gmail.com>
2021-11-18 00:16:36 +0100

    Update routes.js

--- modules/teacher/routes.js
+++ modules/teacher/routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-
+const auth = require('../../middleware/auth')
+const authTeacher = require('../../middleware/teacherAuth')
 const teacher = require('./handler')
 
 router.get('/promote/:id', [auth, authTeacher], teacher.promote)
