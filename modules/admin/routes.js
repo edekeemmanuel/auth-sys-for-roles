55b8a1feb6535454a2066ec80192215e347db12f
Abiola Fasanya <hezekiahfasanya@gmail.com>
2021-11-18 11:06:15 +0100

    Update routes.js

--- modules/admin/routes.js
+++ modules/admin/routes.js
@@ -1,13 +1,5 @@
-const express = require("express");
-const router = express.Router();
-const admin = require("./handler");
-const auth = require("../../middleware/auth")
-const authRole = require("../../middleware/adminAuth")
-
-// @route   POST /admin/signin
-router.post("/signin", admin.signin);
-// router.post("/create-user", admin.createUser)
-router.get("/create-super-admin", admin.superAdmin);
-router.get("/dashboard", auth, authRole, admin.dashboard)
-
-module.exports = router;
+const route = require('express').Router();
+route.get("/admin", (req, res) => {
+    res.send("admin");
+})
+module.exports = route;
