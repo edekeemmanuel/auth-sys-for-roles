cc4f159e8e51a1d8ff307a1e6bab42918ede4600
Abiola Fasanya <hezekiahfasanya@gmail.com>
2021-11-18 11:21:58 +0100

    Update routes.js

--- modules/user/routes.js
+++ modules/user/routes.js
@@ -1,12 +1,3 @@
 const router = require("express").Router();
-const user = require("./handler");
-const auth = require("../../middleware/auth")
-const authAdmin = require("../../middleware/adminAuth")
-
-router.post("/signin", user.signin);
-router.post("/signup", auth, authAdmin, user.createUser);
-router.get("/profile/:id", auth, user.getProfile);
-router.get("/verify", user.verify);
-router.delete('/delete/:id', auth, authAdmin, user.deleteUser)
 
 module.exports = router;
